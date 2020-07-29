e980942669de344e092fe8102ffc72428369a0d3
蒋生平 <jiang.shengping@tslsmart.com>
2020-07-29 19:09:43 +0800

    update

--- mysql/mysql5.7/privileges.sql
+++ mysql/mysql5.7/privileges.sql
@@ -1,11 +1,12 @@
 use mysql;
 select host, user from user;
+-- 默认超级权限
 create user root identified by 'zp*anLu0R5J';
 grant all on *.* to root@'%' identified by 'zp*anLu0R5J' with grant option;
 
 -- 因为mysql版本是5.7，因此新建用户为如下命令：
-create user docker identified by '123456';
+--create user docker identified by '123456';
 -- 将docker_mysql数据库的权限授权给创建的docker用户，密码为123456：
-grant all on docker_mysql.* to docker@'%' identified by '123456' with grant option;
+--grant all on docker_mysql.* to docker@'%' identified by '123456' with grant option;
 -- 这一条命令一定要有：
 flush privileges;
